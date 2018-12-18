b9b9ed1a8fc8a5c021b0ac70bdbc571c9d215d00
da Vinci <brucewilson1103@gmail.com>
2018-12-17 21:12:04 -0500

    finally got the buttons to output to total score, but they are being input as strings so i will need to parsint them i think

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,48 @@
 var wins = 0;
 var losses = 0;
-var cpunum = Math.floor(Math.random() * (120-19 +1) +19);
+var totalnumber = 0;
+var cpunum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
 console.log(cpunum);
- $(".targetnumber").html(cpunum);
+$(".targetnumber").html(cpunum);
 $("#wins").html("wins:" + wins);
 $("#losses").html("losses: " + losses);
 
-$(".crystal").each(function () {  
+$(".crystal").each(function () {
   // console.log($(this))
-  var randomnumber = Math.floor(Math.random() * (12-1 +1) +1);
+  var randomnumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
   $(this).attr("data-value", randomnumber)
   // Math.floor(Math.random() * (120-19 +1) +19); put in variable and add attributes to the divs
   // var ran
-})
+});
+  // If the button is clicked add the data value attribute assigned to the image to the total number variable
+  $(document).ready(function () {
+    $('#diamondBtn').on('click', addtobox);
+    
+    function addtobox() {$(".totalnumber").append($("#diamond").attr("data-value"));}
+    // var totalnumber = totalnumber++$(this).attr.parsInt(randomnumber)}
+
+  });
+
+  $(document).ready(function () {
+    $('#purpleBtn').on('click', addtobox);
+    
+    function addtobox() {$(".totalnumber").append($("#tigereye").attr("data-value"));}
+    // var totalnumber = totalnumber++$(this).attr.parsInt(randomnumber)}
+
+  });
+
+  $(document).ready(function () {
+    $('#brownBtn').on('click', addtobox);
+    
+    function addtobox() {$(".totalnumber").append($("#browncrystal").attr("data-value"));}
+    // var totalnumber = totalnumber++$(this).attr.parsInt(randomnumber)}
+
+  });
+
+  $(document).ready(function () {
+    $('#greenBtn').on('click', addtobox);
+    
+    function addtobox() {$(".totalnumber").append($("#greencrystal").attr("data-value"));}
+    // var totalnumber = totalnumber++$(this).attr.parsInt(randomnumber)}
+
+  });
