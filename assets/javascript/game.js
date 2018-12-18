1a13d79457472b09b31259d3260da696446f3730
da Vinci <brucewilson1103@gmail.com>
2018-12-18 16:15:57 -0500

    finally

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,77 @@
+
+$(document).ready(function() {
 var wins = 0;
 var losses = 0;
 var totalnumber = 0;
 var cpunum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-var currenttotal = 0
+var currenttotal = 0;
 console.log(cpunum);
 $(".targetnumber").html(cpunum);
 $(".totalnumber").html("");
 $("#losses").html("losses: " + losses);
 $("#wins").html("wins:" + wins);
+  console.log(cpunum +"this is in doc ready");
 
+  function assignCrystalValue() {
+    $(".crystal").each(function () {
+      // console.log($(this))
+      var randomnumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+      $(this).attr("data-value", randomnumber)
+    
+      // Math.floor(Math.random() * (120-19 +1) +19); put in variable and add attributes to the divs
+      // var ran
+    });
+  }
+  function newgame () {
+    assignCrystalValue();
+    cpunum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    console.log(cpunum +"this is in new game");
+    $(".targetnumber").html(cpunum);
+    $(".totalnumber").html(0);
+    $("#wins").html("wins:" + wins);
+    currenttotal=0;
 
+    
+  };
 
-function assignCrystalValue() {
-  $(".crystal").each(function () {
-    // console.log($(this))
-    var randomnumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    $(this).attr("data-value", randomnumber)
-    // Math.floor(Math.random() * (120-19 +1) +19); put in variable and add attributes to the divs
-    // var ran
-  });
-}
-
-function newgame () {
-  assignCrystalValue();
-  
-  $(".totalnumber").html(0);
-  $("#wins").html("wins:" + wins);
-  currenttotal=0;
-  var cpunum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-  $(".targetnumber").html(cpunum);
- 
-}
-
-
-$(document).ready(function() {
-  
   assignCrystalValue();
 
   $(".crystal").on('click', function() {
     // get value of crystal
+      
     var crystalNumber = $(this).attr("data-value");
     crystalNumber = parseInt(crystalNumber);
+
+     
   
     currenttotal += crystalNumber;
   
     $(".totalnumber").text(currenttotal);
-
+    console.log("adding number");console.log(currenttotal);
+    console.log("cpumatch"); console.log(cpunum);
     if (cpunum === currenttotal) {
 
       wins ++;
       $("#wins").html("wins:" + wins);
-      newgame ();
       alert("You have won!")
-    }
+      newgame();
+      console.log(cpunum +"this is in win condition")
+    };
   
     if (cpunum < currenttotal) {
   
       losses ++;
       $("#losses").html("losses: " + losses);
-      newgame ();
-      alert("You have lost!")
-    }
+      alert("You have lost!");
+      newgame();
+      
+    };
   
   });
 
 })
+
+
 
   // If the button is clicked add the data value attribute assigned to the image to the total number variable
   // $(document).ready(function () {
